cdebab9a27af115d8cae59561de49e6ff119168f
MichaelH94 <michaelhaggard94@gmail.com>
2018-11-17 13:39:30 -0600

    added sequelize database routes

--- models/hltb.js
+++ models/hltb.js
@@ -1,3 +1,12 @@
 let hltb = require('howlongtobeat');
 let hltbService = new hltb.HowLongToBeatService();
+let placeholder = "dark souls"
+let timeToBeat = function hltbSearch(game) {
+    hltbService.search(game).then(result => {
+        console.log(result)
 
+    })
+};
+
+timeToBeat(placeholder)
+module.exports = timeToBeat;
---
+++ models/users.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      // Giving the Users model a name of type STRING
+        username: DataTypes.STRING,
+        password: DataTypes.STRING,
+        name: DataTypes.STRING,
+        age:  {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            len: [1,2]
+        },
+        location: DataTypes.STRING,
+        style: DataTypes.INTEGER,
+        genre: DataTypes.INTEGER,
+        platform: DataTypes.INTEGER
+    });
+  
+    User.associate = function(models) {
+      // Associating User with Games
+      // When a User is deleted, also delete any associated Games
+      User.hasMany(models.Games, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return User;
+  };
+
