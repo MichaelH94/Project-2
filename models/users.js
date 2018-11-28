e3ab66264ac9d472af3c1df2ddd173ed172b045f
Heeb <iheberj@gmail.com>
2018-11-27 21:23:55 -0600

    finished basic login page layout

--- models/users.js
+++ models/users.js
@@ -1,28 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-      // Giving the Users model a name of type STRING
-        username: DataTypes.STRING,
-        password: DataTypes.STRING,
-        name: DataTypes.STRING,
-        age:  {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            len: [1,2]
-        },
-        location: DataTypes.STRING,
-        style: DataTypes.INTEGER,
-        genre: DataTypes.INTEGER,
-        platform: DataTypes.INTEGER
+  var User = sequelize.define("User", {
+    // Giving the Users model a name of type STRING
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      len: [1, 2]
+    },
+    location: DataTypes.STRING,
+    style: DataTypes.INTEGER,
+    genre: DataTypes.INTEGER,
+    platform: DataTypes.INTEGER
+  });
+
+  User.associate = function(models) {
+    // Associating User with Games
+    // When a User is deleted, also delete any associated Games
+    User.hasMany(models.Games, {
+      onDelete: "cascade"
     });
-  
-    User.associate = function(models) {
-      // Associating User with Games
-      // When a User is deleted, also delete any associated Games
-      User.hasMany(models.Games, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return User;
   };
 
+  return User;
+};
