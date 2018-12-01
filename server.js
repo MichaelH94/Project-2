b6cbe4281c72f9bdf71cfe81b5287d7f966bab6e
Alex Young <alexyoungcontact@gmail.com>
2018-12-01 12:39:14 -0600

    fixed being able to run node server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // app.use(express.static("views"));
 
 // Routes
-//const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes")
 
 app.use('/', htmlRoutes)
