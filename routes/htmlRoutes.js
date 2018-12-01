2a03828978d39e907edacff8610b46825b6c8570
Heeb <iheberj@gmail.com>
2018-12-01 10:09:19 -0600

    adding the api post request

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,19 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  // Load index Page
+  app.get('/', (req, res) => {
+    res.render('index');
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  //Render login page
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  //render create account
+  app.get('/creatAccount', (req, res) => {
+    res.render('createAccount');
   });
 
   // Render 404 page for any unmatched routes
