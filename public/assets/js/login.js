03a4a76fc42b4bd24410008dc47fccfb371a46b1
zxcv1337x <michaelhaggard94@gmail.com>
2018-12-03 17:14:34 -0600

    working create account modal, just needs to link to homepage

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,7 +1,29 @@
-$(document).on('click', '#loginAccount', loginToAccount);
-
-let loginToAccount = () => {
+let handleCreateAccount = () => {
   event.preventDefault();
 
+  let username = $('#createUsername').val().trim();
+  let name = $('#name').val().trim();
+  let password = $('#createPassword').val();
+  let age = $('#age').val().trim();
+  let location = $('#location').val().trim();
+  let style = $('#playStyle').val();
+  let platform = $('#platform').text();
 
-};
+  addUser({
+    username: username,
+    name: name,
+    password: password,
+    age: age,
+    location: location,
+    style: style,
+    platform: platform
+  });
+
+}
+
+let addUser = (userData) => {
+  $.post('/create-account', userData);
+}
+
+$('#createAccount').on('click', handleCreateAccount);
+
