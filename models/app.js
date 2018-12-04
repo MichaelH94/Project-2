2eddda627717a336cab0752148dcdac151af0a80
Heeb <iheberj@gmail.com>
2018-12-03 20:57:47 -0600

    get route: still needs parsing

--- models/app.js
+++ models/app.js
@@ -1,124 +1,143 @@
 const connection = require('../config/connection');
-const sequelize = require ('sequelize')
+const sequelize = require('sequelize')
 
 var User = connection.define("User", {
-    // Giving the Users model a name of type STRING
-      username: sequelize.STRING,
-      password: sequelize.STRING,
-      name: sequelize.STRING,
-      age:  {
-          type: sequelize.INTEGER,
-          allowNull: false,
-          len: [1,2]
-      },
-      location: sequelize.STRING,
-      style: sequelize.INTEGER,
-      platform: sequelize.STRING
+  // Giving the Users model a name of type STRING
+  username: sequelize.STRING,
+  password: sequelize.STRING,
+  name: sequelize.STRING,
+  age: {
+    type: sequelize.INTEGER,
+    allowNull: false,
+    len: [1, 2]
+  },
+  location: sequelize.STRING,
+  style: sequelize.INTEGER,
+  platform: sequelize.STRING
+});
+
+var Games = connection.define("Games", {
+  username: sequelize.STRING,
+  name: sequelize.STRING,
+  imageUrl: {
+    type: sequelize.STRING,
+    isUrl: true,
+    allowNull: true
+  },
+  timetobeat: sequelize.INTEGER,
+  // commenting out the genre, unable to make it work and wasted too much time
+  // genre: sequelize.STRING,
+  summary: sequelize.TEXT,
+  hypes: sequelize.INTEGER,
+  rating: sequelize.INTEGER,
+  esrb: sequelize.STRING
+});
+
+var Posts = connection.define("Posts", {
+  body: {
+    type: sequelize.TEXT,
+    allowNull: false
+  }
+});
+
+User.associate = () => {
+  User.hasMany(Games, {
+    onDelete: "cascade"
   });
+};
 
-    var Games = connection.define("Games", {
-      username: sequelize.STRING,
-      name: sequelize.STRING,
-      imageUrl: {
-        type: sequelize.STRING,
-        isUrl: true,
-        allowNull: true
-      },
-      timetobeat: sequelize.INTEGER,
-      // commenting out the genre, unable to make it work and wasted too much time
-      // genre: sequelize.STRING,
-      summary: sequelize.TEXT,
-      hypes: sequelize.INTEGER,
-      rating: sequelize.INTEGER,
-      esrb: sequelize.STRING
-    });
+User.associate = () => {
+  User.hasMany(Posts, {
+    onDelete: "cascade"
+  })
+};
 
-    var Posts = connection.define("Posts", {
-        body: {
-            type: sequelize.TEXT,
-            allowNull: false
-        }
-    });
+Games.associate = () => {
+  // We're saying that a Games should belong to an User
+  // A Games can't be created without an User due to the foreign key constraint
+  Games.belongsTo(User, {
+    foreignKey: {
+      allowNull: false
+    }
+  });
+};
 
-    User.associate = () => {
-        User.hasMany(Games, {
-            onDelete: "cascade"
-        });
-    };
-
-    User.associate = () => {
-        User.hasMany(Posts, {
-            onDelete: "cascade"
-        })
-    };
-
-    Games.associate = () => {
-      // We're saying that a Games should belong to an User
-      // A Games can't be created without an User due to the foreign key constraint
-      Games.belongsTo(User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-
-    Posts.associate = () => {
-        Posts.belongsTo(User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
+Posts.associate = () => {
+  Posts.belongsTo(User, {
+    foreignKey: {
+      allowNull: false
+    }
+  });
+};
 
 let app = {
-createUser: (username, password, name, age, location, style, platform) => {
-  console.log("Made it in here");
+  createUser: (username, password, name, age, location, style, platform) => {
+    console.log("Made it in here");
     connection.sync().then(() => {
-        User.create({
-            username: username,
-            password: password,
-            name: name,
-            age: age,
-            location: location,
-            style: style,
-            platform: platform
-        });
+      User.create({
+        username: username,
+        password: password,
+        name: name,
+        age: age,
+        location: location,
+        style: style,
+        platform: platform
+      });
 
-        return;
+      return;
     });
-},
+  },
 
-createGame: (name, imageUrl, timetobeat, summary, hypes, rating, esrb) => {
+  getUser: (username) => {
+    console.log('made it to getUser!!!');
     connection.sync().then(() => {
-        Games.create({
-            name: name,
-            imageUrl: imageUrl,
-            timetobeat: timetobeat,
-            summary: summary,
-            hypes: hypes,
-            rating: rating,
-            esrb: esrb,
-        });
+      User.findOne({ where: { username: username } }).then(user => {
+        return user;
+      });
     });
-},
+  },
 
-deleteUser: (user) => {
+  createGame: (name, imageUrl, timetobeat, summary, hypes, rating, esrb) => {
     connection.sync().then(() => {
-        User.destroy({
-            where: {
-                username: user
-            }
-        })
+      Games.create({
+        name: name,
+        imageUrl: imageUrl,
+        timetobeat: timetobeat,
+        summary: summary,
+        hypes: hypes,
+        rating: rating,
+        esrb: esrb,
+      });
     });
-    },
+  },
 
-addPost: (data) => {
+  deleteUser: (user) => {
     connection.sync().then(() => {
-        Posts.create({
-            body: data
-        })
+      User.destroy({
+        where: {
+          username: user
+        }
+      })
+    });
+  },
+
+  addPost: (data) => {
+    connection.sync().then(() => {
+      Posts.create({
+        body: data
+      })
     })
-}
+  }
 }
 
 module.exports = app;
+
+
+/*
+ getUser: (username) => {
+  connection.sync().then() => {
+    User.findOne({ where: { username: username } }).then(user => {
+      return user;
+    });
+    }
+    */
